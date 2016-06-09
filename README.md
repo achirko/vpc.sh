67c2d936240375c766a4954e565128a62fd71bc4
Oleksandr Chyrko <oleksandrc@backbase.com>
2016-06-09 12:34:01 +0300

    Ready for pypi

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-CLI tool to run shell commands on ec2 instances. Built with glorious click, boto and fabric.
+# CLI tool to run shell commands on ec2 instances.
 
 ## Supported cloud platforms
 
@@ -10,11 +10,9 @@ CLI tool to run shell commands on ec2 instances. Built with glorious click, boto
 - [x] Multiple remote users (if can't authenticate - try next username)
 - [x] Filter by tags
 - [x] Parallel execution
-- [ ] Filter by launched date (`launched-before` and `launched-after` )
+- [ ] Filter by launch date (`launched-before` and `launched-after` )
 - [ ] Filter by keypair
 - [ ] Profiles (group settings and filters in named profile, and specify profile on vpc.sh run)
-- [ ] `vpc.sh list` command - similar to `vpc.sh run`, but only prints info about instances
 - [ ] heredocs-style multiple commands
-- [ ] PyPI
 
 ## Usage
--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    readme = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    readme = open('README.md').read()
+
 setup(
     name='vpc.sh',
     version='0.1',
+    description="CLI tool to run shell commands on ec2 instances.",
+    long_description=readme,
+    url="https://github.com/achirko/vpc.sh",
     author='Oleksandr Chyrko',
     author_email='aleksandr.chirko@gmail.com',
     py_modules=['vpc_sh'],
@@ -10,5 +19,17 @@ setup(
     entry_points='''
         [console_scripts]
         vpc.sh=vpc_sh:vpc_sh
-    '''
+    ''',
+    classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Clustering',
+          'Topic :: System :: Systems Administration',
+    ],
 )
